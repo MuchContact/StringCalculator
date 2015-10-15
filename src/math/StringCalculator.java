6c7d4c87abf600627d6b41820c9a2632833dd19a
hjliu <hjliu@thoughtworks.com>
2015-10-15 13:27:35 +0800

    add test for multi lines input, different delimiters, negative check.

--- src/math/StringCalculator.java
+++ src/math/StringCalculator.java
@@ -1,10 +1,42 @@
 package math;
 
+import java.security.InvalidParameterException;
+import java.util.Arrays;
+
 public class StringCalculator {
-    public StringCalculator(String source) {
+
+    public StringCalculator() {
     }
 
-    public int result() {
-        return 0;
+    public int add(String source) {
+
+        if (source.trim().equals(""))
+            return 0;
+
+        String delimiter = ",";
+        final String starter = "//;\n";
+        if (source.startsWith(starter)) {
+            delimiter = ";";
+            source = source.substring(starter.length());
+        }
+        final String newLine = "\n";
+        if (source.contains(delimiter + newLine)) {
+            throw new InvalidParameterException("Invalid input contains '," + newLine + "'");
+        }
+        source = source.replace(newLine, delimiter);
+
+        if (hasNegatives(source, delimiter)) {
+            throw new InvalidParameterException("negatives not allowed " + getNegativeString(source, delimiter));
+        }
+        return Arrays.stream(source.split(delimiter)).mapToInt(Integer::valueOf).sum();
+    }
+
+    private String getNegativeString(String source, String delimiter) {
+        return Arrays.toString(
+                Arrays.stream(source.split(delimiter)).filter(p -> p.contains("-")).toArray());
+    }
+
+    private boolean hasNegatives(String source, String delimiter) {
+        return Arrays.stream(source.split(delimiter)).filter(p -> p.contains("-")).count() > 0;
     }
 }
--- test/math/StringCalculatorTest.java
+++ test/math/StringCalculatorTest.java
@@ -1,14 +1,79 @@
 package math;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.security.InvalidParameterException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class StringCalculatorTest {
+
+    private final StringCalculator calc = new StringCalculator();
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
     @Test
-    public void should_add_handle_empty_string() throws Exception {
-        StringCalculator calc = new StringCalculator("");
-        assertThat(calc.result(), is(0));
+    public void should_add_empty_string() throws Exception {
+        assertThat(calc.add(""), is(0));
+    }
+
+    @Test
+    public void should_add_string_with_one_number() throws Exception {
+        assertThat(calc.add("3"), is(3));
+    }
+
+    @Test
+    public void should_get_12_when_add_string_with_two_nums_4_8() throws Exception {
+        assertThat(calc.add("4,8"), is(12));
+    }
+
+    @Test
+    public void should_get_5_when_add_string_with_two_nums_2_3() throws Exception {
+        assertThat(calc.add("2,3"), is(5));
+    }
+
+    @Test
+    public void should_get_6_when_add_three_nums_1_2_3() throws Exception {
+        assertThat(calc.add("1,2,3"), is(6));
+    }
+
+    @Test
+    public void should_handle_multi_lines() throws Exception {
+        assertThat(calc.add("1\n2,3"), is(6));
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    public void should_give_exception_when_error_multi_lines() throws Exception {
+        calc.add("1,\n2,3");
+    }
+
+    @Test
+    public void should_use_another_delimiter() throws Exception {
+        assertThat(calc.add("//;\n2;3"), is(5));
+        assertThat(calc.add("//;\n6;0"), is(6));
+    }
+
+    @Test
+    public void should_check_negative_1() throws Exception {
+        exception.expect(InvalidParameterException.class);
+        exception.expectMessage("negatives not allowed [-1]");
+        calc.add("-1");
+    }
+
+    @Test
+    public void should_check_negative_2() throws Exception {
+        exception.expect(InvalidParameterException.class);
+        exception.expectMessage("negatives not allowed [-2]");
+        calc.add("-2");
+    }
+
+    @Test
+    public void should_check_multi_negatives() throws Exception {
+        exception.expect(InvalidParameterException.class);
+        exception.expectMessage("negatives not allowed [-2, -1]");
+        calc.add("3,-2,-1");
     }
 }
